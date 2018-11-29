db873d085839ee2bf93e50d0e3e3ee965f2b35a2
ravidDx <raulbastidas@outlook.com>
2018-11-28 20:58:09 -0500

    creacion formulario producto

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Producto;
+use App\Form\ProductoType;
+use App\Repository\ProductoRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +14,26 @@ class ProductoController extends AbstractController
     /**
      * @Route("/producto", name="producto")
      */
-    public function index()
+    public function index(ProductoRepository $productoRepository)
     {
-        return $this->render('producto/index.html.twig', [
-            'controller_name' => 'ProductoController',
+
+    	$form_new= $this->getFormNew();
+
+         return $this->render('producto/index.html.twig', [
+            'form_new'=>$form_new->createView(),
+            'productos' => $productoRepository->findAll()
         ]);
     }
+
+
+
+    public function getFormNew(){ 
+
+        $producto = new Producto();
+        $form = $this->createForm(ProductoType::class, $producto);
+
+        return $form;
+
+    }
+
 }
--- src/Form/ProductoType.php
+++ src/Form/ProductoType.php
@@ -1,29 +1,37 @@
 <?php
+
 namespace App\Form;
-use App\Entity\Cliente;
+
+use App\Entity\Producto;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-class ClienteType extends AbstractType
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class ProductoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('barcode',TextType::class)
             ->add('nombre',TextType::class)
-            ->add('email',TextType::class)
-            ->add('direccion',TextareaType::class)
-            ->add('cedula',TextType::class)
-            ->add('telefono',TextType::class)
+            ->add('cantPack',IntegerType::class)
+            ->add('cantUnit',IntegerType::class)
+            ->add('cantTotal',IntegerType::class)
+            ->add('precioPack',IntegerType::class)
+            ->add('precioUnit',IntegerType::class)
+            ->add('isIva',CheckboxType::class)
             ->add('guardar', SubmitType::class)
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Cliente::class,
+            'data_class' => Producto::class,
         ]);
     }
 }
